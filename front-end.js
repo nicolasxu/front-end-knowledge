6040bd8b1038ae9ac8d5b6a1f9e846ade6af38e6
Nicolas Xu <xu.shenxin@gmail.com>
2015-12-10 17:27:44 -0800

    update

--- front-end.js
+++ front-end.js
@@ -1691,6 +1691,17 @@ function frontEnd () {
 	function Libraries () {
 		function jquery() {
 
+			function throttle_and_debounce() {
+				var source = "http://benalman.com/projects/jquery-throttle-debounce-plugin/";
+				var github = "https://github.com/cowboy/jquery-throttle-debounce/";
+
+				// They all take in a function and a param, then returns a function.
+				// Debounce absorb limited the number of calls.
+				// Throttle absort time intervals. 
+				$(window).resize( $.throttle( 250, resize_2 ) ); // This is the line you want!
+				$('input:text').keyup( $.debounce( 250, ajax_lookup ) );
+			}
+
 			function remove_all_classes() {
 				$("#item").removeClass(); 
 				// if no param, all classes will be removed
