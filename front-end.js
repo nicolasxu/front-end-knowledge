46366fa1b18968001932206b068b2bfe5b81d5d7
Shenxin(Nicolas) Xu <xu.shenxin@gmail.com>
2015-12-17 13:51:24 -0800

    updates

--- front-end.js
+++ front-end.js
@@ -2831,6 +2831,22 @@ function frontEnd () {
 				  });
 			}
 
+			function html5mode() {
+				var what_is_html5mode = "https://docs.angularjs.org/guide/$location";
+
+				module.config(function($locationProvider){
+					$locationProvider.html5Mode(true);
+					// will enable html5Mode
+					// also need to add <base href="/"> to index.html
+				});
+
+				// Server side
+				// Using HTML5 mode requires URL rewriting on server side
+				// basically you have to rewrite all your links to entry point 
+				// of your application (e.g. index.html)
+				
+			}
+
 			function ui_router() {
 				var source = "https://angular-ui.github.io/ui-router/site/#/api/ui.router";
 				var guide = "https://github.com/angular-ui/ui-router/wiki";
