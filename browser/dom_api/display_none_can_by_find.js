277f1313a47c6362d060183bad02ce1754a973c7
Shenxin(Nicolas) Xu <xu.shenxin@gmail.com>
2016-03-08 22:22:08 -0800

    update

---
+++ browser/dom_api/display_none_can_by_find.js
@@ -0,0 +1,16 @@
+/*
+#noshow{
+	display: none;
+}
+*/
+	/*
+	<div id="noshow">
+		this is a modal dialog
+		<!-- <button type="" id="toggleModalDialog"></button> -->
+	</div>
+	*/
+
+		var displayNoneElem = document.getElementById("noshow");
+
+// It still can be found by getElementById, and other dom api. 
+	
