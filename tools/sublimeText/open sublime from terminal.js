040b6ae1777eb1a073dd8098c3a3f4c63494dbd2
Polar Bottle <shenxinxu@Shens-MacBook-Pro.local>
2016-06-18 11:44:44 -0400

    update

---
+++ tools/sublimeText/open sublime from terminal.js
@@ -0,0 +1,12 @@
+
+var source = "http://ashleynolan.co.uk/blog/launching-sublime-from-the-terminal";
+// step 1
+// open /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl
+
+// to see if that works
+
+// step 2
+// ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+
+// step 3
+// use "sublime ." to open current folder as project
